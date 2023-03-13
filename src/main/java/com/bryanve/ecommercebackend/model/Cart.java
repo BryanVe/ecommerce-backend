071f634682371d69f94f9bbe5fe100f8eca2d003
Bryan Vera <bryan.ve.bv@gmail.com>
2023-03-13 16:58:37 -0500

    feat: add ttl of 10 minutes to carts

--- src/main/java/com/bryanve/ecommercebackend/model/Cart.java
+++ src/main/java/com/bryanve/ecommercebackend/model/Cart.java
@@ -3,19 +3,13 @@ package com.bryanve.ecommercebackend.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 
-import java.time.Instant;
 import java.util.List;
 
 public record Cart(@JsonProperty("id") int id,
-                   @JsonProperty("createdAt") Instant createdAt,
                    @NotEmpty @JsonProperty("productIDs") List<Integer> productIDs) {
 
     public int getId() {
         return id;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
     }
 
     public List<Integer> getProductIDs() {
